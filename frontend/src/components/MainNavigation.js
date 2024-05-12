f1c632a0b66b18df19f4da82fcb21ef49238c265
sshhhgit <seungho0497@hanmail.net>
2024-05-13 08:56:53 +0900

    -

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
 
@@ -8,10 +8,24 @@ function MainNavigation() {
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/Events">Events</Link>
+            <NavLink to="/Events"
+                     className={({ isActive }) =>
+                         isActive ? classes.active : undefined
+                     }
+            >
+              Events
+            </NavLink>
           </li>
         </ul>
       </nav>
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,5 +1,27 @@
+import * as events from "events";
+import {Link} from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  {
+  id: 'e1',
+  title: 'Some evnet',
+  },  
+  {
+  id: 'e2',
+  title: 'Another envet',
+  },
+]
 function EventsPage() {
-  return <h1>EventsPage</h1>;
+  return (
+      <>
+      <h1>EventsPage</h1>
+      <ul>
+        {DUMMY_EVENTS.map(event => <li key={event.id}>
+          <Link to={event.id}></Link>
+            </li>)}
+      </ul>
+      </>
+  );
 }
 
 export default EventsPage;
