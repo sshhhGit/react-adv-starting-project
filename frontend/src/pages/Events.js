d8b3ac7d1c7ceb76048b8a7b0d22256d28676366
sshhhgit <seungho0497@hanmail.net>
2024-05-20 08:49:55 +0900

    udemy practice

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,4 +1,4 @@
-// import {useLoaderData} from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 
 import EventsList from '../components/EventsList';
 
@@ -13,3 +13,15 @@ function EventsPage() {
 }
 
 export default EventsPage;
+
+export async function loader() {
+
+    const response = await fetch('http://localhost:8080/events');
+
+    if (!response.ok) {
+        // ...
+    } else {
+        const resData = await response.json();
+        return resData.events;
+    }
+}
