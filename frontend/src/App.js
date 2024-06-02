8abb65c69322cb7ae2ede9b8f244e95fa52f9daa
sshhhgit <seungho0497@hanmail.net>
2024-06-03 08:52:08 +0900

    udemy practice

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,15 +24,14 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import HomePage from "./pages/Home";
-import EventsPage, { loader as eventsLoader} from "./pages/Events";
-import EventDetailPage from "./pages/EventDetail";
-import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
-import RootLayout from "./pages/Root";
-import EventsRoot from "./pages/EventsRoot";
+import ErrorPage from "./pages/Error";
+import EventDetailPage, { loader as eventDetailLoader } from "./pages/EventDetail";
+import EventsPage, { loader as eventsLoader} from "./pages/Events";
 import EventsRootLayout from "./pages/EventsRoot";
-import ErrorPage from "./pages/ErrorPage";
+import HomePage from "./pages/Home";
+import NewEventPage,  from "./pages/NewEvent";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
   {
@@ -49,7 +48,7 @@ const router = createBrowserRouter([
             element: <EventsPage />,
             loader: eventsLoader,
             },
-          { path: ":eventId", element: <EventDetailPage /> },
+          { path: ":eventId", element: <EventDetailPage />, loader: eventDetailLoader },
           { path: "new", element: <NewEventPage /> },
           { path: ":eventId/edit", element: <EditEventPage /> },
         ] 
--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,13 +1,26 @@
-import {useParams} from "react-router-dom";
 import EventItem from "../components/EventItem";
+import { useLoaderData,json} from "react-router-dom";
 
 function EventDetailPage() {
-  const params = useParams();
+  const data = useLoaderData();
   
   return (
-      <EventItem event={}/>
+      <EventItem event={data.event}/>
   );
 }
 
 export default EventDetailPage;
 
+export  async function loader({request, params}){
+  const id = params.eventId;
+  
+  const response = await fetch('http://localhost:8080/events/' + id);
+  
+  if (!response.ok) {
+    throw json({message: 'Could not fetch derails for selected event.'}, {
+      status: 500
+    })
+  } else {
+    return response;
+  }
+}
